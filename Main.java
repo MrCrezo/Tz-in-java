095c6bb8d1bea7b298d8a35e588d95586de1cb37
MrCrezo <153993774+MrCrezo@users.noreply.github.com>
2024-07-20 11:37:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 2 значения в двойных кавычках для строк!");
+        System.out.println("При делении и умножении второе значение обязательно цифра!");
+        System.out.println("Между значениями один из знаков операции /, *, -, + пробел перед знаком и после!");
+        String exp = sc.nextLine();
+        char action;
+        String[] data;
+
+
+        if (exp.contains("+")) {
+            data = exp.split(" \\+ ");
+            action = '+';
+        } else if (exp.contains("-")) {
+            data = exp.split(" - ");
+            action = '-';
+        } else if (exp.contains("*")) {
+            data = exp.split(" \\* ");
+            action = '*';
+        } else if (exp.contains("/")) {
+            data = exp.split(" / ");
+            action = '/';
+        } else {
+            throw new Exception("Некорректный знак действия");
+        }
+
+        if (action == '+' || action == '-') {
+            for (String datum : data) {
+                if (!datum.startsWith("\"") || !datum.endsWith("\"")) {
+                    throw new Exception("Значения должны быть в двойных кавычках");
+                }
+            }
+        }
+
+        if (action == '*' || action == '/') {
+            if (!data[0].startsWith("\"") || !data[0].endsWith("\"")) {
+                throw new Exception("Значения должны быть в двойных кавычках");
+            }
+            if (data[1].contains("\"")) {
+                throw new Exception("Строку можно делить или умножать только на число");
+            }
+            if (data[1].equals("0")) {
+                throw new ArithmeticException("Деление на ноль невозможно");
+            }
+            try {
+                Integer.parseInt(data[1]);
+            } catch (NumberFormatException e) {
+                throw new Exception("Второе значение должно быть числом");
+            }
+        }
+        int value = Integer.parseInt(data[1]);
+        if (value == 0 || (value > 10)) {
+            throw new Exception("Число может быть только от 1 до 10");
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = data[i].replace("\"", "");
+        }
+        switch (action) {
+            case '+':
+                addition(data);
+                break;
+            case '-':
+                subtraction(data);
+                break;
+            case '*':
+                multiplication(data);
+                break;
+            case '/':
+                division(data);
+                break;
+        }
+
+    }
+    static void addition(String[] data) {
+        printInQuotes(data[0] + data[1]);
+        }
+    static void subtraction(String[] data) {
+        int index = data[0].indexOf(data[1]);
+        if (index == -1) {
+            printInQuotes(data[0]);
+        } else {
+            String result = data[0].substring(0, index);
+            result += data[0].substring(index + data[1].length());
+            printInQuotes(result);
+        }
+    }
+    static void multiplication(String[] data) {
+         int multiplier = Integer.parseInt(data[1]);
+         String result = "";
+         for (int i = 0; i < multiplier; i++) {
+             result += data[0];
+         }
+         printInQuotes(result);
+    }
+    static void division(String[] data) {
+        int newLen = data[0].length() / Integer.parseInt(data[1]);
+        String result = data[0].substring(0, newLen);
+        printInQuotes(result);
+        }
+
+    static void printInQuotes(String text) {
+        if (text.length() > 40) {
+            String result = text.substring(0, 40);
+            System.out.println(result + "...");
+        } else {
+            System.out.println("\"" + text + "\"");
+        }
+    }
+}
